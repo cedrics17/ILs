27cc1ad0f876fa39e1f64f78c8d32447cace3f03
cedrics17 <105883173+cedrics17@users.noreply.github.com>
2022-05-20 13:13:44 -0400

    Update get_flux.py

--- get_flux.py
+++ get_flux.py
@@ -15,12 +15,14 @@ import argparse
 parser         = argparse.ArgumentParser()
 
 # -b Begin -e End -t initial timestep -T temperature                                                                                  
+parser.add_argument("-p","--pair",dest="pair", help="pair")
 parser.add_argument("-b","--begin", dest= "begin", help=" First Snapshot")
 parser.add_argument("-e","--end", dest="end", help="last Snapshot")
 parser.add_argument("-T","--temperature",dest="temperature",help="temperature")
+
 args           = parser.parse_args()
 hours          = "6"
-repname        = "temperature"
+repname        = "_temperature"
 #temperatures   = [300.0, 307.595, 315.381, 323.365, 331.551, 339.945, 348.55, 357.374, 366.421, 375.697, 385.208, 394.959, 404.958, 415.209,425.72,436.497]                                                                                                                                      
 temperatures   = [300.0, 323.365, 348.55, 375.697, 404.958, 436.497]
 name_middle    = "_snapshot" # 750 snapshots from each trajectory
@@ -32,16 +34,16 @@ snapshots_beg  = int(args.begin)
 snapshots_end  = int(args.end)
 timesteps      = 500
 temp           = float(args.temperature)
-ogpath         ="/projectnb/nonadmd/cedric17/proj2b_lammps/4-pair_P111-DCA/replica/SingleTraj_P111-DCA/"
-key            ='key'
+pair           = args.pair
+key            ='neat' # change if salted. There are two other options: "same salt" or "diff salt"
 
 os.chdir('../') #bring this back when running superscript
 
-time_array=de.grabtime(repname+str(round(temp))+'/'+'dump'+str(snapshots_beg)+'.lammpstrj')
-os.chdir(repname+str(round(temp)))
+time_array=de.grabtime(pair+repname+str(round(temp))+'/'+'dump'+str(snapshots_beg)+'.lammpstrj')
+os.chdir(pair+repname+str(round(temp)))
 
 for i in range(snapshots_beg,snapshots_end):
-        out=open(ogpath+repname+str(round(temp))+'/flux'+str(i)+'.dat','w')
+        out=open(pair+repname+str(round(temp))+'/flux'+str(i)+'.dat','w')
         out.write('Time (fs)   Jx (e*Ang/fs)   Jy (e*Ang/fs)   Jz (e*Ang/fs)\n')
 
         J=de.splitflux('dump'+str(i)+'.lammpstrj',key)
