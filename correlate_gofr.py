2cb840b9cf596443f4a00041f3701b56c7d9bafe
cedrics17 <105883173+cedrics17@users.noreply.github.com>
2023-03-31 16:11:21 -0400

    Update correlate_gofr.py

--- correlate_gofr.py
+++ correlate_gofr.py
@@ -76,6 +76,71 @@ def splitflux(filename,key): #filename is a variable that represents a string
                         else:
                                 print("Error in the order of the ions and the formatting of the dump files")
         return J
+def molsplitflux(filename,key): #filename is a variable that represents a string
+        Ncat=100
+        qcat=1
+        Nani=100
+        qani=-1
+        Nlit=0
+        count=0
+        catarray=[]
+        anaarray=[]
+        saltanarray=[]
+        litharray=[]
+        start     =False
+        J         =[]
+        
+        file=open(filename,'r').readlines()
+        for f in file:
+                if 'id' in f:
+                        start=True 
+                elif start:
+                        array=f.strip().split()
+                        if str(array[0])=="ITEM:":
+                                start=False
+                                anaarray=np.array(anaarray)
+                                catarray=np.array(catarray)
+                                litharray=np.array(litharray)
+                                saltanarray=np.array(saltanarray)
+                                [Jx_cat,Jy_cat,Jz_cat]=flux(catarray[:,0],catarray[:,1],catarray[:,2],catarray[:,3],Ncat,qcat)
+                                [Jx_ani,Jy_ani,Jz_ani]=flux(anaarray[:,0],anaarray[:,1],anaarray[:,2],anaarray[:,3],Nani,qani)
+                                if key=='diff salt' or key=="same salt":
+
+                                        [Jx_salt,Jy_salt,Jz_salt]=flux(saltanarray[:,0],saltanarray[:,1],saltanarray[:,2],saltanarray[:,3],Nlit,qani)
+                                        [Jx_lith,Jy_lith,Jz_lith]=flux(litharray[:,0],litharray[:,1],litharray[:,2],litharray[:,3],Nlit,qcat)
+                                else:
+                                        Jx_salt=0
+                                        Jx_lith=0
+                                        Jy_salt=0
+                                        Jy_lith=0
+                                        Jz_salt=0
+                                        Jz_lith=0
+                                Jcat.append([Jx_cat,Jy_cat,Jz_cat])
+				Jani.append([Jx_ani,Jy_ani,Jz_ani])
+				Jsalt.append([Jx_salt,Jy_salt,Jz_salt])
+				Jlith.append([Jx_lith,Jy_lith,Jz_lith])
+                                J.append([Jx_cat+Jx_ani+Jx_salt+Jx_lith, Jy_cat+Jy_ani+Jy_salt+Jy_lith, Jz_cat+Jz_ani+Jz_salt+Jz_lith])
+                                catarray=[]
+                                anaarray=[]
+                                saltanarray=[]
+                                Nlit=0
+                                litharray=[]
+                        elif int(array[1])<=100:
+                                temp=[mass_dic[array[3]],float(array[11]),float(array[12]),float(array[13])]
+                                catarray.append(temp)
+                        elif int(array[1])>100 and int(array[1])<=200:
+                                temp=[mass_dic[array[3]],float(array[11]),float(array[12]),float(array[13])]
+                                anaarray.append(temp)        
+                        elif int(array[1])>200 and array[3]!="Li":
+                                temp=[mass_dic[array[3]],float(array[11]),float(array[12]),float(array[13])]
+                                saltanarray.append(temp)
+                        elif array[3]=="Li":
+                                temp=[mass_dic[array[3]],float(array[11]),float(array[12]),float(array[13])]
+                                litharray.append(temp)
+                                Nlit+=1
+                        else:
+                                print("Error in the order of the ions and the formatting of the dump files")
+        return J, Jcat, Jani, Jsalt, Jlith
 
 def grabtime(filename):
         timestart=False
